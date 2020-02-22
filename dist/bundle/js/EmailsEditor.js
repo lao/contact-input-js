af5a7846ad1e2918dc276d15ea66f418bc6732f2
Lucas Oliveira <lucasabreuoliveira@gmail.com>
2020-02-22 19:42:40 +0100

    testing sandbox ready

--- dist/bundle/js/EmailsEditor.js
+++ dist/bundle/js/EmailsEditor.js
@@ -10,19 +10,55 @@
     }
   }
 
-  var isDomElement = function isDomElement() {
-    console.log('test');
-  };
-  var Polyfill = {
-    isDomElement: isDomElement
-  };
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
 
-  var emailsEditor = function emailsEditor() {
-    _classCallCheck(this, emailsEditor);
-    alert('test');
-    console.log('test');
-    console.log(Polyfill.isDomElement(null));
-  };
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
+
+  var emailsEditor =
+  function () {
+    function emailsEditor(config) {
+      _classCallCheck(this, emailsEditor);
+      var _ref = config || {},
+          container = _ref.container;
+      if (!this.validConfig(config)) {
+        console.log('config invalid');
+      }
+      this.emails = {};
+      this.render(container);
+      var input = container.querySelector('input');
+      var listener = function listener(e) {
+        return e;
+      };
+      input.addEventListener('change', listener);
+      input.addEventListener('keyup', listener);
+    }
+    _createClass(emailsEditor, [{
+      key: "validConfig",
+      value: function validConfig(config) {
+        return config && config.container;
+      }
+    }, {
+      key: "render",
+      value: function render(el) {
+        var template = "\n      <div class=\"container\">\n        <div class=\"top\">\n          <h1>Share <strong>Board name</strong> with others</h1>\n          <div class=\"text-container\">\n            <div class=\"ballons-container\">\n              <div class=\"ballon\">dfdff</div>\n            </div>\n            <input placeholder=\"add more people...\"/>\n          </div>\n        </div>\n        <div class=\"bottom\">\n          <button>\n            Add email\n          </button>  \n          <button>\n            Get emails count\n          </button>\n        </div>\n      </div>\n    ";
+        el.innerHTML = template;
+        return el;
+      }
+    }]);
+    return emailsEditor;
+  }();
 
   return emailsEditor;
 
--- src/emailsEditor.js
+++ src/emailsEditor.js
@@ -1,14 +1,57 @@
 /* eslint-disable no-console */
-
-import { Polyfill } from './helpers';
 import './main.scss';
 
 export default class emailsEditor {
-  constructor() {
-    alert('test');
-    // eslint-disable-next-line no-console
-    console.log('test');
-    console.log(Polyfill.isDomElement(null));
+  constructor(config) {
+    const {
+      container,
+      // onChange,
+      // getEmails,
+      // setEmails
+    } = config || {};
+
+    if (!this.validConfig(config)) {
+      console.log('config invalid');
+    }
+
+    this.emails = {};
+
+    this.render(container);
+    const input = container.querySelector('input');
+    const listener = (e) => {
+      return e;
+    };
+    input.addEventListener('change', listener);
+    input.addEventListener('keyup', listener);
+  }
+
+  validConfig(config) {
+    return config && config.container;
+  }
+
+  render(el) {
+    const template = `
+      <div class="container">
+        <div class="top">
+          <h1>Share <strong>Board name</strong> with others</h1>
+          <div class="text-container">
+            <div class="ballons-container">
+              <div class="ballon">dfdff</div>
+            </div>
+            <input placeholder="add more people..."/>
+          </div>
+        </div>
+        <div class="bottom">
+          <button>
+            Add email
+          </button>  
+          <button>
+            Get emails count
+          </button>
+        </div>
+      </div>
+    `;
+    el.innerHTML = template;
+    return el;
   }
 }
-
