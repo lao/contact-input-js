dec886919a507c025b5a1bf78d0678cb0e2ef7ca
Lucas Oliveira <lucasabreuoliveira@gmail.com>
2020-02-23 18:34:44 +0100

    getting counts, removing items, set and get emails

--- dist/bundle/js/EmailsEditor.js
+++ dist/bundle/js/EmailsEditor.js
@@ -39,24 +39,49 @@
       this.initValues(config).renderMainContainer().initListeners();
     }
     _createClass(emailsEditor, [{
-      key: "destroy",
-      value: function destroy() {
-      }
-    }, {
       key: "initValues",
       value: function initValues(config) {
         this.emails = {};
         this.onChange = config.onChange;
         this.container = config.container;
+        this.onGetCount = config.onGetCount;
+        return this;
+      }
+    }, {
+      key: "getEmails",
+      value: function getEmails() {
+        return Object.keys(this.emails);
+      }
+    }, {
+      key: "setEmails",
+      value: function setEmails(emails) {
+        var _this = this;
+        if (!emails || !emails.length) {
+          return;
+        }
+        var emailsTemp = emails.reduce(function (email, nonRendered) {
+          if (_this.isValidEmail(email)) {
+            nonRendered.append(email);
+            _this.emails[email] = true;
+          }
+          return nonRendered;
+        }, []);
+        this.renderEmails(emailsTemp.list);
+      }
+    }, {
+      key: "dispatchOnChange",
+      value: function dispatchOnChange() {
+        console.log('hello');
         return this;
       }
     }, {
       key: "initListeners",
       value: function initListeners() {
-        var _this = this;
+        var _this2 = this;
         var container = this.container;
         var input = container.querySelector('input');
         var ballonsContainer = container.querySelector('.ballons-container');
+        var getCountBtn = container.querySelector('.btn-count');
         var keys = {
           ENTER: 13,
           COMMA: 188
@@ -68,7 +93,8 @@
               ctrlKey = _ref.ctrlKey,
               metaKey = _ref.metaKey;
           if (keyCode === keys.COMMA || keyCode === keys.ENTER) {
-            _this.renderEmails(currentTarget.value).cleanInput();
+            var emailsBefore = Object.keys(_this2.emails);
+            _this2.renderEmails(currentTarget.value).dispatchOnChange(emailsBefore, currentTarget.value).cleanInput();
           }
           if (metaKey || ctrlKey) {
             return false;
@@ -80,42 +106,60 @@
               currentTarget = _ref2.currentTarget,
               clipboardData = _ref2.clipboardData;
           var clipBoardText = clipboardData.getData('text');
-          _this.renderEmails(currentTarget.value + clipBoardText).cleanInput();
+          _this2.renderEmails(currentTarget.value + clipBoardText).cleanInput();
           e.preventDefault();
         };
-        input.addEventListener('paste', pasteListener);
-        input.addEventListener('keyup', keyUpListener);
-        ballonsContainer.addEventListener('click', function (e) {
+        var ballonsClickHandler = function ballonsClickHandler(e) {
           var isClickOnSvg = e.target.getAttribute('class') === 'remove-icon-svg';
           var target = isClickOnSvg ? e.target.parentElement : e.target;
           if (target.className === 'remove-icon') {
-            alert(e.target);
+            if (target.parentElement.className !== 'invalid ballon') {
+              delete _this2.emails[target.parentElement.getAttribute('data-email')];
+            }
             ballonsContainer.removeChild(target.parentElement);
           }
-        });
+        };
+        input.addEventListener('paste', pasteListener);
+        input.addEventListener('keyup', keyUpListener);
+        ballonsContainer.addEventListener('click', ballonsClickHandler);
+        if (this.onGetCount && this.onGetCount instanceof Function) {
+          getCountBtn.addEventListener('click', function () {
+            var emails = Object.keys(_this2.emails);
+            _this2.onGetCount(emails.length);
+          });
+        }
         return this;
       }
     }, {
-      key: "renderEmail",
-      value: function renderEmail(string, valid) {
+      key: "renderEmailBlock",
+      value: function renderEmailBlock(string, valid) {
         var removeIconDiv = document.createElement('div');
         removeIconDiv.className = !valid ? 'invalid ballon' : 'ballon';
         removeIconDiv.innerHTML = "".concat(string, " <div class=\"remove-icon\">").concat(REMOVE_SVG, "</div>");
+        removeIconDiv.setAttribute('data-email', valid ? string : '');
         return removeIconDiv;
       }
     }, {
-      key: "renderEmails",
-      value: function renderEmails(stringValue) {
-        var _this2 = this;
+      key: "isValidEmail",
+      value: function isValidEmail(string) {
         var emailRegx = /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
-        var blocks = (stringValue || '').split(',');
+        return !!emailRegx.exec(string);
+      }
+    }, {
+      key: "renderEmails",
+      value: function renderEmails(stringValue, values) {
+        var _this3 = this;
+        var blocks = values || (stringValue || '').split(',');
         var ballonsBlockFrag = document.createDocumentFragment();
         blocks.map(function (block) {
           if (!(block + '').trim()) {
             return;
           }
-          var results = emailRegx.exec(block);
-          ballonsBlockFrag.appendChild(_this2.renderEmail(block, !!results));
+          var isEmail = _this3.isValidEmail(block);
+          ballonsBlockFrag.appendChild(_this3.renderEmailBlock(block, isEmail));
+          if (isEmail) {
+            _this3.emails[block] = true;
+          }
         });
         var inputEl = this.container.querySelector('.input-container');
         this.container.querySelector('.ballons-container').insertBefore(ballonsBlockFrag, inputEl);
@@ -133,7 +177,7 @@
       key: "renderMainContainer",
       value: function renderMainContainer() {
         var container = this.container;
-        var template = "\n      <div class=\"container\">\n        <div class=\"top\">\n          <h1>Share <strong>Board name</strong> with others</h1>\n          <div class=\"text-container\">\n            <div class=\"ballons-container\">\n              <div class=\"input-container\"><input placeholder=\"add more people...\"/></div>\n            </div>\n          </div>\n        </div>\n        <div class=\"bottom\">\n          <button>\n            Add email\n          </button>  \n          <button>\n            Get emails count\n          </button>\n        </div>\n      </div>\n    ";
+        var template = "\n      <div class=\"container\">\n        <div class=\"top\">\n          <h1>Share <strong>Board name</strong> with others</h1>\n          <div class=\"text-container\">\n            <div class=\"ballons-container\">\n              <div class=\"input-container\"><input placeholder=\"add more people...\"/></div>\n            </div>\n          </div>\n        </div>\n        <div class=\"bottom\">\n          <button>\n            Add email\n          </button>  \n          <button class=\"btn-count\">\n            Get emails count\n          </button>\n        </div>\n      </div>\n    ";
         container.innerHTML = template;
         return this;
       }
--- src/emailsEditor.js
+++ src/emailsEditor.js
@@ -5,6 +5,8 @@ const REMOVE_SVG = `<svg class="remove-icon-svg" width="8" height="8" viewBox="0
 <path fill-rule="evenodd" clip-rule="evenodd" d="M8 0.8L7.2 0L4 3.2L0.8 0L0 0.8L3.2 4L0 7.2L0.8 8L4 4.8L7.2 8L8 7.2L4.8 4L8 0.8Z" fill="#050038"/>
 </svg>`;
 
+// TODO: add random email
+
 export default class emailsEditor {
   constructor(config) {
     config = config || {};
@@ -20,15 +22,39 @@ export default class emailsEditor {
       .initListeners();
   }
 
-  destroy() {
-    // todo!
-    // remove listeners
-  }
-
   initValues(config) {
     this.emails = {};
     this.onChange = config.onChange;
     this.container = config.container;
+    this.onGetCount = config.onGetCount;
+    return this;
+  }
+
+  getEmails() {
+    return Object.keys(this.emails);
+  }
+
+  /**
+   * It receives list of emails and append valids to the local list plus renders valid emails only
+   */
+  setEmails(emails) {
+    if(!emails || !emails.length) {
+      return;
+    }
+    const emailsTemp = emails.reduce((email, nonRendered) => {
+      if(this.isValidEmail(email)) {
+        nonRendered.append(email);
+        this.emails[email] = true;
+      }
+      return nonRendered;
+    }, []);
+    this.renderEmails(emailsTemp.list);
+    // [Decided not to do as setEmails, seems to be useful only for appending]
+    // TODO: remove preexisting emails
+  }
+
+  dispatchOnChange() {
+    console.log('hello');
     return this;
   }
 
@@ -36,6 +62,7 @@ export default class emailsEditor {
     const {container} = this;
     const input = container.querySelector('input');
     const ballonsContainer = container.querySelector('.ballons-container');
+    const getCountBtn = container.querySelector('.btn-count');
     const keys = {
       ENTER: 13,
       COMMA: 188,
@@ -43,8 +70,10 @@ export default class emailsEditor {
     const keyUpListener = e => {
       const { keyCode, currentTarget, ctrlKey, metaKey } = e || {};
       if (keyCode === keys.COMMA || keyCode === keys.ENTER) {
+        const emailsBefore = Object.keys(this.emails);
         this
           .renderEmails(currentTarget.value)
+          .dispatchOnChange(emailsBefore, currentTarget.value)
           .cleanInput();
       }
       if (metaKey || ctrlKey) {
@@ -62,39 +91,64 @@ export default class emailsEditor {
 
       e.preventDefault();
     };
-    input.addEventListener('paste', pasteListener);
-    input.addEventListener('keyup', keyUpListener);
-
-    ballonsContainer.addEventListener('click', (e) => {
+    const ballonsClickHandler = e => {
       const isClickOnSvg = e.target.getAttribute('class') === 'remove-icon-svg';
       const target = isClickOnSvg ? e.target.parentElement : e.target;
 
       if (target.className === 'remove-icon') {
-        alert(e.target);
+        if (target.parentElement.className !== 'invalid ballon') {
+          delete this.emails[target.parentElement.getAttribute('data-email')];
+        }
         ballonsContainer.removeChild(target.parentElement);
       }
-    });
+    };
+
+    input.addEventListener('paste', pasteListener);
+    input.addEventListener('keyup', keyUpListener);
+    ballonsContainer.addEventListener('click', ballonsClickHandler);
+
+    if (this.onGetCount && this.onGetCount instanceof Function) {
+      getCountBtn.addEventListener('click', () => {
+        const emails = Object.keys(this.emails);
+        this.onGetCount(emails.length);
+      });
+    }
+
     return this;
   }
 
-  renderEmail(string, valid) {
+  renderEmailBlock(string, valid) {
     const removeIconDiv = document.createElement('div');
     removeIconDiv.className = !valid ? 'invalid ballon' : 'ballon';
     // TODO: turn this into createElement
     removeIconDiv.innerHTML = `${string} <div class="remove-icon">${REMOVE_SVG}</div>`;
+    removeIconDiv.setAttribute('data-email', valid ? string : '');
     return removeIconDiv;
   }
 
-  renderEmails(stringValue) {
+  isValidEmail(string) {
     // eslint-disable-next-line max-len
     const emailRegx = /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g;
-    const blocks = (stringValue || '').split(',');
+    return !!emailRegx.exec(string);
+  }
+
+  /**
+   * It break string value into blocks by comma, checks if is valid, adds to local copy of valid emails
+   * and then renders, all in one go for performance purposes.
+   */
+  renderEmails(stringValue, values) {
+    const blocks = values || (stringValue || '').split(',');
     const ballonsBlockFrag = document.createDocumentFragment();
 
     blocks.map((block) => {
       if(!(block + '').trim()) {return;}
-      const results = emailRegx.exec(block);
-      ballonsBlockFrag.appendChild(this.renderEmail(block, !!results));
+      const isEmail = this.isValidEmail(block);
+      ballonsBlockFrag.appendChild(this.renderEmailBlock(block, isEmail));
+
+      // validations and saving to local copy
+      if (isEmail) {
+        this.emails[block] = true;
+      }
     });
 
     const inputEl = this.container.querySelector('.input-container');
@@ -112,6 +166,9 @@ export default class emailsEditor {
     return this;
   }
 
+  /**
+   * Renders main html container for editor
+   */
   renderMainContainer() {
     const {container} = this;
     // TODO: turn this to createDocumentFragment later
@@ -129,7 +186,7 @@ export default class emailsEditor {
           <button>
             Add email
           </button>  
-          <button>
+          <button class="btn-count">
             Get emails count
           </button>
         </div>
